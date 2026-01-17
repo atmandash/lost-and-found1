efe62208b9d57a1457032e49aa4eea898b3a0b4b
Atman Dash <websitedeve5@gmail.com>
2026-01-17 16:50:52 +0530

    Fix final syntax error in Profile.jsx closing brackets

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -523,9 +523,8 @@ const Profile = () => {
                         </div>
                     </div>
                 </div>
-            )
-            }
-        </div >
+            )}
+        </div>
     );
 };
 
